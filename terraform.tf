897ea02507744b1189112beede0b2332c182ff60
Remigiusz Dończyk <remigiusz.donczyk@globallogic.com>
2022-07-25 08:58:35 +0200

    temporary fix for overloaded availability zones

--- terraform.tf
+++ terraform.tf
@@ -53,7 +53,8 @@ module "vpc" {
   version              = "3.14.2"
   name                 = "vpc"
   cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
+ #azs                  = data.aws_availability_zones.available.names
+  azs                  = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1f"]
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_nat_gateway   = true
