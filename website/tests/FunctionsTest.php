3f58b7780a1193cbad1e79f2288f616c6a78ad04
Remigiusz Dończyk <remigiusz.donczyk@globallogic.com>
2022-08-08 15:29:04 +0200

    fixed phpunit tests (phpunit still disabled)

--- website/tests/FunctionsTest.php
+++ website/tests/FunctionsTest.php
@@ -154,6 +154,7 @@ final class FunctionsTest extends TestCase {
   }
 
   /**
+   * @uses Functions::countSetBits
    * @covers Functions::getCountMessage
    */
   public function testGetCountMessage(): void {
