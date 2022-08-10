c02f23b8fd0ebf8c870c9f296b0f22257012be60
remigiusz-donczyk <106961710+remigiusz-donczyk@users.noreply.github.com>
2022-08-10 10:12:48 +0200

    Delete website directory

--- website/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-  document.getElementById('docs').addEventListener('click', () => {
-    window.open('docs/index.html', '_self');
-  });
-  document.getElementById('refresh').addEventListener('click', () => {
-    window.location.reload();
-  });
-});
-
