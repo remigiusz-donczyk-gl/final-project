bbc33e69b541f7497684b037589317e5ba00bcb3
Remigiusz Dończyk <remigiusz.donczyk@globallogic.com>
2022-08-22 12:52:00 +0200

    allow mounting

---
+++ website/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sleep 3
+if [ ! -d /var/lib/mysql ] || [ "$(find /var/lib/mysql -type d -empty -exec echo yes \;)" = 'yes' ]; then
+  [ ! -d /var/lib/mysql ] && mkdir /var/lib/mysql
+  cp -r /var/tmp/mysql-initial/** /var/lib/mysql/
+  chown mysql:mysql /var/lib/mysql -R
+fi
+exec /usr/bin/supervisord -c /etc/supervisord.conf
+
