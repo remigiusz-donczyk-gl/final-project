fd9d83a88ba189029c00a13333cf40e7618a8557
Remigiusz Dończyk <remigiusz.donczyk@globallogic.com>
2022-08-24 12:16:25 +0200

    updated doxygen documentation, made a storage class

--- website/Website.php
+++ website/Website.php
@@ -10,7 +10,7 @@ class Website {
 
   /**
    * @brief The main entrypoint method.
-   * @return msg A list of messages to be printed by the website.
+   * @return array A list of messages to be printed by the website.
    *
    * The main entrypoint, runs all functions in the correct order,
    * passes data between them and returns a list of messages to print.
@@ -23,15 +23,15 @@ class Website {
     $olddata = $this->getClientData($db, $ip);
     $data = $this->updateClientData($olddata, $random);
     return array(
-      "count" => $this->getCountMessage($data["Seen"], $memes),
-      "try"   => $this->getTryMessage($data["Tries"]),
+      "count" => $this->getCountMessage($data->Seen, $memes),
+      "try"   => $this->getTryMessage($data->Tries),
       "image" => $this->getImageEmbed($db, $random)
     );
   }
 
   /**
    * @brief Creates a connection to the database.
-   * @return db The database object to access.
+   * @return mysqli The database object to access.
    *
    * Creates a connection to a 'website' local database as user 'dbuser',
    * with the default mysqli password (set in the ini file). Returns a mysqli object.
@@ -42,8 +42,8 @@ class Website {
 
   /**
    * @brief Counts set bits in an int.
-   * @param[in] $n   The integer to count bits in.
-   * @return    seen The number of bits set to 1.
+   * @param  $n  The integer to count bits in.
+   * @return int The number of bits set to 1.
    *
    * Counts set bits in an integer, this is how I have decided to store the memes already seen by a user;
    * each bit represents the 'seen' state of the meme at the same index in the database,
@@ -61,8 +61,8 @@ class Website {
 
   /**
    * @brief Gets the length of the meme table.
-   * @param[in] $db  The database to access.
-   * @return    rows The total amount of memes.
+   * @param  $db The database to access.
+   * @return int The total amount of memes.
    *
    * Gets all memes from the database and counts the number of rows.
    */
@@ -74,16 +74,16 @@ class Website {
 
   /**
    * @brief Gets the IP of a user.
-   * @return ip The IP of the connecting client.
+   * @return string The IP of the connecting client.
    *
    * Accesses server headers in order to get the most accurate reading possible of the user's IP.
    * Tries more accurate methods before falling back on less precise, but more readily available ones.
    */
   function getRealClientIP(): string {
     if (!empty($_SERVER["HTTP_CLIENT_IP"])) {
-         $ip = $_SERVER["HTTP_CLIENT_IP"];
+      $ip = $_SERVER["HTTP_CLIENT_IP"];
     } elseif (!empty($_SERVER["HTTP_X_FORWARDED_FOR"])) {
-               $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+      $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
     } else {
       $ip = $_SERVER["REMOTE_ADDR"];
     }
@@ -92,76 +92,64 @@ class Website {
 
   /**
    * @brief Creates a client object in the database.
-   * @param[in] $db  The database to connect to.
-   * @param[in] $ip  The IP of the user, used as an identifier.
-   * @return    data The newly-created client data.
+   * @param  $db      The database to connect to.
+   * @param  $ip      The IP of the user, used as an identifier.
+   * @return UserData The newly-created client data.
    *
    * Connects to the database and creates a client object with default parameters.
    * Returns the database, IP, 'seen' memes (see countSetBits), and the amount of tries.
    * Since this is a new client, both 'seen' memes and tries are 0.
    */
-  function createClientData(mysqli $db, string $ip): array {
+  function createClientData(mysqli $db, string $ip): UserData {
     $query = "INSERT INTO userdata (IP) VALUES ('" . $ip . "')";
     $db->query($query);
-    return array(
-      "Database" => $db,
-      "IP"       => $ip,
-      "Seen"     => 0,
-      "Tries"    => 0
-    );
+    return new UserData($db, $ip, 0, 0);
   }
 
   /**
    * @brief Gets client data from the database.
-   * @param[in] $db  The database to connect to.
-   * @param[in] $ip  The IP of the user, used as an identifier.
-   * @return    data The client data object.
+   * @param  $db      The database to connect to.
+   * @param  $ip      The IP of the user, used as an identifier.
+   * @return UserData The client data object.
    *
    * Connects to the database and retrieves data stored with a matching identifier.
    */
-  function getClientData(mysqli $db, string $ip): array {
+  function getClientData(mysqli $db, string $ip): UserData {
     $query = "SELECT Seen, Tries FROM userdata WHERE IP='" . $ip . "'";
-    $userdata = $db->query($query);
-    if ($userdata->num_rows == 0) {
+    $data = $db->query($query);
+    if ($data->num_rows == 0) {
       return $this->createClientData($db, $ip);
     }
-    return array_merge(array(
-      "Database" => $db,
-      "IP"       => $ip,
-    ), $userdata->fetch_array());
+    $row = $data->fetch_array();
+    return new UserData($db, $ip, intval($row["Seen"]), intval($row["Tries"]));
   }
 
   /**
    * @brief Updates client data with the newly-seen meme.
-   * @param[in] $data The client data to update.
-   * @param[in] $meme The randomly-generated meme index.
-   * @return    data  The updated version of client data.
+   * @param  $data    The client data to update.
+   * @param  $meme    The randomly-generated meme index.
+   * @return UserData The updated version of client data.
    *
    * User data includes the database they are stored in,
    * so the data is updated locally as well as in the database,
    * and returned in the same format as the input.
    */
-  function updateClientData(array $data, int $meme): array {
-    $newseen = $data["Seen"] | (1 << ($meme - 1));
-    $newtries = $data["Tries"] + 1;
+  function updateClientData(UserData $data, int $meme): UserData {
+    $newseen = $data->Seen | (1 << ($meme - 1));
+    $newtries = $data->Tries + 1;
     if ($newtries > 999) {
       $newtries = 999;
     }
-    $query = "UPDATE userdata SET Seen=" . $newseen . ", Tries=" . $newtries . " WHERE IP='" . $data["IP"] . "'";
-    $data["Database"]->query($query);
-    return array(
-      "Database" => $data["Database"],
-      "IP"       => $data["IP"],
-      "Seen"     => $newseen,
-      "Tries"    => $newtries
-    );
+    $query = "UPDATE userdata SET Seen=" . $newseen . ", Tries=" . $newtries . " WHERE IP='" . $data->IP . "'";
+    $data->Database->query($query);
+    return new UserData($data->Database, $data->IP, $newseen, $newtries);
   }
 
   /**
    * @brief Gets the meme to display.
-   * @param[in] $db The database to access.
-   * @param[in] $n  Index to the meme to fetch.
-   * @return    img The image tag with the correct path.
+   * @param  $db    The database to access.
+   * @param  $n     Index to the meme to fetch.
+   * @return string The image tag with the correct path.
    *
    * Accesses the database to fetch a meme, and creates an img HTML tag with the path to it.
    */
@@ -173,9 +161,9 @@ class Website {
 
   /**
    * @brief Gets the count of seen memes message.
-   * @param[in] $seen  The user's seen memes integer.
-   * @param[in] $total The total count of memes in the database.
-   * @return    msg    The message to display on the page.
+   * @param  $seen  The user's seen memes integer.
+   * @param  $total The total count of memes in the database.
+   * @return string The message to display on the page.
    *
    * Checks the amount of memes seen and returns 'x/y memes seen' or,
    * if all memes were seen, 'all memes seen' message.
@@ -190,8 +178,8 @@ class Website {
 
   /**
    * @brief Gets the amount of tries message.
-   * @param[in] $tries The number of tries by user.
-   * @return    msg    The message to disaply on the page.
+   * @param  $tries The number of tries by user.
+   * @return string The message to display on the page.
    *
    * Checks the amount of tries, and displays a 'x tries so far' or,
    * if the number of tries excedds 999, a 'too many times' message.
@@ -201,6 +189,19 @@ class Website {
       return "<p>You have tried <i>too many</i> times, seriously.</p>";
     }
     return "<p>You have tried " . $tries . " times so far.</p>";
+  }
+}
+
+class UserData {
+  public mysqli $Database;
+  public string $IP;
+  public int $Seen;
+  public int $Tries;
+  function __construct(mysqli $db, string $ip, int $seen, int $tries) {
+    $this->Database = $db;
+    $this->IP = $ip;
+    $this->Seen = $seen;
+    $this->Tries = $tries;
   }
 }
 
